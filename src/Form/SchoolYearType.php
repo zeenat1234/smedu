37e3720f406025546ae11ef3b87d40c896acff26
Dan Alexandru Pod <dj.diablo.x@gmail.com>
2018-07-06 21:46:20 +0100

    Finished School year entity base form layout - styling still required

---
+++ src/Form/SchoolYearType.php
@@ -0,0 +1,58 @@
+<?php
+
+namespace App\Form;
+
+use App\Entity\SchoolYear;
+use Symfony\Component\Form\AbstractType;
+use Symfony\Component\Form\FormBuilderInterface;
+use Symfony\Component\OptionsResolver\OptionsResolver;
+
+#this is used for forms
+use Symfony\Component\Form\Extension\Core\Type\TextType;
+use Symfony\Component\Form\Extension\Core\Type\DateTimeType;
+use Symfony\Component\Form\Extension\Core\Type\DateType;
+use Symfony\Component\Form\Extension\Core\Type\CheckboxType;
+
+# Symfony4 best practice is to not use a submit type in the formType or Controller
+#use Symfony\Component\Form\Extension\Core\Type\SubmitType;
+
+class SchoolYearType extends AbstractType
+{
+    public function buildForm(FormBuilderInterface $builder, array $options)
+    {
+        $builder
+            ->add('yearLabel', TextType::class, array(
+              'attr' => array('class' => 'col-12 form-control'),
+              'required' => true,
+            ))
+            ->add('startDate', DateType::class, array(
+              'input' => 'datetime',
+              'widget' => 'single_text',
+              'attr' => array('type' => 'datetime', 'class' => 'col-6 form-control'),
+            ))
+            ->add('endDate', DateType::class, array(
+              'input' => 'datetime',
+              'widget' => 'single_text',
+              'attr' => array('type' => 'datetime', 'class' => 'col-6 form-control'),
+            ))
+            ->add('is_perm_activity', CheckboxType::class, array(
+              'label'    => 'Activitate Permanentă?',
+              'required' => false,
+              'attr' => array('class' => 'form-check form-check-inline'),
+            ))
+            ->add('license', TextType::class, array(
+              'attr' => array('class' => 'col-4 form-control'),
+            ))
+            ->add('license_status', TextType::class, array(
+              'attr' => array('class' => 'col-4 form-control'),
+            ))
+        ;
+    }
+
+    public function configureOptions(OptionsResolver $resolver)
+    {
+        $resolver->setDefaults([
+            'data_class' => SchoolYear::class,
+        ]);
+    }
+}
