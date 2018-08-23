2c73445147eacdf75dc3f2a2f7eb4ff1be9c3257
Dan Alexandru Pod <dj.diablo.x@gmail.com>
2018-08-23 02:34:55 +0100

    Updated module controller with an UNDER CONSTRUCTION page

--- src/Controller/ClassModuleController.php
+++ src/Controller/ClassModuleController.php
@@ -2,18 +2,41 @@
 
 namespace App\Controller;
 
-use Symfony\Component\Routing\Annotation\Route;
-use Symfony\Bundle\FrameworkBundle\Controller\Controller;
+#can instantiate the entity
+use App\Entity\ClassModule;
+use App\Entity\SchoolUnit;
+use App\Entity\SchoolYear;
+use App\Entity\User;
 
-class ClassModuleController extends Controller
+use Symfony\Component\Routing\Annotation\Route;
+use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
+
+#allows us to restrict methods like get and post
+use Sensio\Bundle\FrameworkExtraBundle\Configuration\Method;
+
+#form type definition
+//use App\Form\ClassOptionalType;
+//use App\Form\ClassOptionalEnrollType;
+
+use Symfony\Component\HttpFoundation\Request;
+use Symfony\Component\HttpFoundation\Response;
+
+class ClassModuleController extends AbstractController
 {
     /**
      * @Route("/class/modules", name="class_modules")
      */
     public function index()
     {
+        $currentSchoolYear = $this->getDoctrine()->getRepository
+        (SchoolYear::class)->findCurrentYear();
+
+        $schoolUnits = $currentSchoolYear->getSchoolunits();
+
         return $this->render('class_module/class.modules.html.twig', [
-            'controller_name' => 'ClassModuleController',
+            'current_year'  => $currentSchoolYear,
+            'current_units' => $schoolUnits,
         ]);
+
     }
 }
